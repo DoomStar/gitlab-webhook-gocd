507404abc53ad16fbe7b867949b6e1973ac5b516
Spiridonov <Semen.Spiridonov@ncloudtech.ru>
2016-02-29 17:34:42 +0300

    init

--- GitHookEvent.py
+++ GitHookEvent.py
@@ -77,28 +77,25 @@ class GitHookEvent(object):
 
         url = urlparse.urlparse(req['path'])
         param = urlparse.parse_qs(url.query)
-        print param
-        print param['gocd_profile']
-        print gocd_profiles.keys()
         if 'gocd_profile' in param.keys():
-            if param['gocd_profile'] in gocd_profiles.keys():
-                gocd = gocd_profiles[ param['gocd_profile'] ]
+            if param['gocd_profile'][0] in gocd_profiles.keys():
+                gocd = gocd_profiles[ param['gocd_profile'][0] ]
             else:
-                print "Error: gocd profile "+param['gocd_profile']+" not found in list of profiles: "+', '.join(gocd_profiles.keys())
+                print "Error: gocd profile "+param['gocd_profile'][0]+" not found in list of profiles: "+', '.join(gocd_profiles.keys())
                 return
 
         elif 'host' in param.keys() and 'port' in param.keys() and 'user' in param.keys() and 'pass' in param.keys():
             gocd = {
-                'url': 'http://'+param['host']+':'+param['port'],
-                'user': param['user'],
-                'pass': param['pass']
+                'url': 'http://'+param['host'][0]+':'+param['port'][0],
+                'user': param['user'][0],
+                'pass': param['pass'][0]
             }
 
         else:
             print "Error: gocd profile not defined"
             return
 
-        print "GO.CD API: curl -u '"+gocd['user']+":"+gocd['pass']+"' -X POST --data 'materials["+param['material']+"]="+req['sha']+"&variables[GIT_TAG]="+req['tag']+"' "+gocd['url']+"/go/api/pipelines/"+param['pipeline']+"/schedule"
+        print "GO.CD API: curl -u '"+gocd['user']+":"+gocd['pass']+"' -X POST --data 'materials["+param['material'][0]+"]="+req['sha']+"&variables[GIT_TAG]="+req['tag']+"' "+gocd['url']+"/go/api/pipelines/"+param['pipeline'][0]+"/schedule"
 
     def get_default_config_path(selfs):
         return './githookevent.conf.json'
